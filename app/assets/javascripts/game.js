cb82b0b0806b2cf358b1cffe0f8fbf1d7cb2b97c
Paul McGarry <paulmcgarry78@hotmail.com>
2017-01-16 16:41:02 +0000

    added fix 3

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -13,3 +13,14 @@ game.state.add('tutorial', tutorialState);
 game.state.add('defence', defenceState);
 game.state.add('reset', resetState);
 game.state.start('boot');
+
+/*global game*/
+var bootState = {
+    preload: function() {
+        //game.load.image('star', 'assets/star.png');
+        game.load.image('preloadBar', '/assets/preloadBar.png');
+    },
+    create: function() {
+        game.state.start('preload');
+    }
+};
